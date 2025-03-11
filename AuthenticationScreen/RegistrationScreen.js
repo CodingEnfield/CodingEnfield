aad4acc05d0b99a07bcb4b5a02af7e9688d00a78
CodingEnfield <codingsenfield@gmail.com>
2025-03-11 08:38:09 +0530

    Created registration page with form and validation

---
+++ AuthenticationScreen/RegistrationScreen.js
@@ -0,0 +1,12 @@
+import Recat from 'react';
+import { View,Text } from 'react-native';
+
+const RegistrationScreen=()=>{
+    return(
+        <View>
+            <Text>In RegistrationScreen</Text>
+        </View>
+    )
+}
+
+export default RegistrationScreen;
